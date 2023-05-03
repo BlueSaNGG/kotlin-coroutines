3dc49c2b1d5668b4bfb72e426144df4fa0e474b3
BlueSaNGG <747182080@qq.com>
2023-05-03 17:35:53 -0500

    Update PlantListViewModel.kt
    enable the filter

--- advanced-coroutines-codelab/start/src/main/java/com/example/android/advancedcoroutines/PlantListViewModel.kt
+++ advanced-coroutines-codelab/start/src/main/java/com/example/android/advancedcoroutines/PlantListViewModel.kt
@@ -23,6 +23,8 @@ import androidx.lifecycle.asLiveData
 import androidx.lifecycle.switchMap
 import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.Job
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.flatMapLatest
 import kotlinx.coroutines.launch
 
 /**
@@ -72,6 +74,15 @@ class PlantListViewModel internal constructor(
     }
 
     val plantsUsingFlow: LiveData<List<Plant>> = plantRepository.plantsFlow.asLiveData()
+    private val growZoneFlow = MutableStateFlow<GrowZone>(NoGrowZone)
+
+    val plantsUsingFlow: LiveData<List<Plant>> = growZoneFlow.flatMapLatest { growZone ->
+        if (growZone == NoGrowZone) {
+            plantRepository.plantsFlow
+        } else {
+            plantRepository.getPlantsWithGrowZoneFlow(growZone)
+        }
+    }.asLiveData()
 
     init {
         // When creating a new ViewModel, clear the grow zone and perform any related udpates
@@ -89,6 +100,7 @@ class PlantListViewModel internal constructor(
      */
     fun setGrowZoneNumber(num: Int) {
         growZone.value = GrowZone(num)
+        growZoneFlow.value = GrowZone(num)
 
         // initial code version, will move during flow rewrite
         launchDataLoad { plantRepository.tryUpdateRecentPlantsCache() }
@@ -102,6 +114,7 @@ class PlantListViewModel internal constructor(
      */
     fun clearGrowZoneNumber() {
         growZone.value = NoGrowZone
+        growZoneFlow.value = NoGrowZone
 
         // initial code version, will move during flow rewrite
         launchDataLoad { plantRepository.tryUpdateRecentPlantsCache() }
